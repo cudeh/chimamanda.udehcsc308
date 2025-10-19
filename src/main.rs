8f517da0a66707bfaa0fbb6cccf083976d5a4417
Chimamanda Udeh <chimamanda.udeh@pau.edu.ng>
2025-10-19 23:04:29 +0100

    My second commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+<<<<<<< HEAD
     let bill_amount: f64 = 12000.0; 
     let mut discount: f64 = 0.0;    
 
@@ -14,4 +15,15 @@ fn main() {
     println!("Original Bill: ₦{}", bill_amount);
     println!("Discount applied: {}%", discount * 100.0);
     println!("Final Bill after discount: ₦{}", final_amount);
+=======
+    // classwork 1
+    println!("Hello, world!");
+
+    let celsius = 38.7;
+    let converted = (celsius * 9.0/5.0) + 32.0;
+
+    println!("previous temperature in celsius is: {} " , celsius);
+    println!("after converting to fahrenheit: {}", converted);
+
+>>>>>>> ebd962a (My second commit)
 }
